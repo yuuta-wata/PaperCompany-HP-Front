7bd6864cf6f418d1316b568fdb8284d55577a464
yuuta <yuuta_cookie@icloud.com>
2020-02-08 19:48:20 +0900

    ヘッダー作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Header() {
+  return (
+    <header>
+      <h1>PaperCompany</h1>
+      <nav>
+        <ul className="nav-list">
+          <li>Home</li>
+          <li>Service</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
